b3a171927a809de0e4d9ffb64b35929d46002ee9
Pritesh Kumar <pritesh.kumar2@gmail.com>
2025-06-06 10:45:29 +0530

    add thunks

---
+++ src/features/posts/PostList.js
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postsSlice";
+import PostExcerpt from "./PostExcerpt";
+
+function PostList() {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+
+  let content;
+  if(postsStatus === 'loading'){
+    content = <p>Loading...</p>;
+  }
+  else if(postsStatus === 'succeeded'){
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  content = orderedPosts.map((post) => (
+    <PostExcerpt key={post.id} post={post}/>
+  ));
+  }
+  else if(postsStatus === 'failed'){
+    content = <p>{error}</p>;
+  }
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+}
+
+export default PostList;
